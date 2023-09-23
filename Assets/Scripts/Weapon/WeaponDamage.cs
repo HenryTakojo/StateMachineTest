f6871f3812e0a1f76fcea5dd7727d2dfaa4a8113
HenryTakojo <123970226+HenryTakojo@users.noreply.github.com>
2023-09-23 09:06:29 +0800

    Attack

---
+++ Assets/Scripts/Weapon/WeaponDamage.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    private float lifeTime = 2.0f;
+    private float currentTime = 0;
+
+    [SerializeField] private Collider myCollider;
+
+    private int damage;
+
+    private List<Collider> alreadyCollidedWith = new List<Collider>();
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        PhysicsCollide();
+
+        currentTime += Time.deltaTime;
+        if (currentTime > lifeTime)
+        {
+            if (this.name == "IceArrow(Clone)")
+            {
+                Destroy(gameObject);
+                currentTime = 0;
+            }
+            this.gameObject.SetActive(true);
+            currentTime = 0;
+        }
+
+        transform.position += transform.forward * 20 * Time.deltaTime;
+    }
+
+    private void OnEnable()
+    {
+        alreadyCollidedWith.Clear();
+    }
+
+    private void PhysicsCollide()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.1f);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject.CompareTag("Enemy"))
+            {
+                if (alreadyCollidedWith.Contains(collider)) { return; }
+                alreadyCollidedWith.Add(collider);
+
+                DealDamage(collider.gameObject, collider);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //適用於角色身上有武器也有collider，可套用在丘丘人
+        //if(collision == myCollider) { return; }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (alreadyCollidedWith.Contains(collision)) { return; }
+            alreadyCollidedWith.Add(collision);
+
+            DealDamage(collision.gameObject, collision);
+        }
+    }
+
+    private void DealDamage(GameObject Enemy, Collider other)
+    {
+        // Enemy Health Decrease
+        if(other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damage);
+        }
+    }
+    public void SetAttack(int damage)
+    {
+        this.damage = damage;
+    }
+}
